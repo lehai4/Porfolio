9556169f952084a31af1106baecd5fcb1799996e
lehai4 <86140878+lehai4@users.noreply.github.com>
2024-08-29 23:03:35 +0700

    fix bug hidden contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -5,7 +5,7 @@ import Title from "@/app/components/Title";
 const Contact = () => {
   return (
     <div className="min-h-[100vh] h-full">
-      <div className="pt-[108px] lg:px-[113px] pb-[31px]">
+      <div className="pt-[108px] lg:px-[113px]">
         <div className="container">
           <Title>
             <h2 className="mb-[50px] lg:mb-[78px] text-center text-[22px] sm:text-[25px] md:text-[30px] lg:text-[38px] font-bold text-white">
