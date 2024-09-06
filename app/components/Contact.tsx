90dcaf1726e6c13ec6fb80f082ca3d1de8eb2087
lehai4 <86140878+lehai4@users.noreply.github.com>
2024-09-06 09:14:14 +0700

    fixing reveal contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,18 +1,20 @@
 import FormContact from "@/app/components/Form";
 import ScrollReveal from "@/app/components/ScrollReveal";
-import Title from "@/app/components/Title";
+import { option, options } from "@/app/utils/optionHandler";
 
 const Contact = () => {
   return (
     <div className="min-h-[100vh] h-full">
       <div className="pt-[108px] lg:px-[113px]">
         <div className="container">
-          <h2 className="mb-[50px] lg:mb-[78px] text-center text-[22px] sm:text-[25px] md:text-[30px] lg:text-[38px] font-bold text-white">
-            Contact <span className="text-primary-color">Me</span>
-          </h2>
-          <FormContact />
-          {/* <ScrollReveal>
-          </ScrollReveal> */}
+          <ScrollReveal options={option}>
+            <h2 className="mb-[50px] lg:mb-[78px] text-center text-[22px] sm:text-[25px] md:text-[30px] lg:text-[38px] font-bold text-white">
+              Contact <span className="text-primary-color">Me</span>
+            </h2>
+          </ScrollReveal>
+          <ScrollReveal options={options}>
+            <FormContact />
+          </ScrollReveal>
         </div>
       </div>
     </div>
---
+++ app/utils/optionHandler.ts
@@ -0,0 +1,18 @@
+export const options = {
+  reset: true,
+  duration: 900,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out",
+};
+export const option = {
+  reset: true,
+  duration: 3000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10,
+  },
+};
